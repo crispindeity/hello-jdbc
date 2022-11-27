eb4e2c50f0ab25df1215757ec773cc9c9f7f82d0
crispindeity <shoy1415@gmail.com>
2022-11-27 23:15:10 +0900

    V0 Test

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -0,0 +1,78 @@
+package hello.jdbc.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static hello.jdbc.connection.DBConnectionUtil.getConnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class MemberRepositoryV0Test {
+
+    private Member member;
+
+    @Test
+    void given_when_then() throws Exception {
+        String sql = "insert into member(member_id, money) values (?, ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+
+    }
+
+
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+}
+
